ee1b6d9b7dae7b01720e9e6840936a91be8019d6
matheusizaias <matheusi.silva@hotmail.com>
2021-06-03 16:29:08 -0300

    estoque quase la

--- lib/views/Stock/form_stock/stock.dart
+++ lib/views/Stock/form_stock/stock.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import 'package:badydoces/models/categoria.model.dart';
 import 'package:badydoces/models/produto.model.dart';
 import 'package:badydoces/repositories/categoria_repository.dart';
@@ -22,25 +24,41 @@ class _StockState extends State<Stock> {
     var repositoryProduct =
         Provider.of<ProductRepository>(context, listen: true);
     var categorias = repositoryCategory.categorias;
+    Iterable<Product> produtosCat;
+    List<Product> auxiliar;
     var produtos = repositoryProduct.products;
-    var auxProdutos = produtos;
-
-    for (var c in auxProdutos) {
-      print(c.category);
-    }
 
     if (itemSelecionado != null) {
-      print(auxProdutos);
-      for (var c in auxProdutos) {
-        print(c.category);
-      }
-      for (var x in auxProdutos) {
-        print('object1222');
-        if (x.category == itemSelecionado.name.toString()) {
-          print('object');
-          produtos = repositoryProduct.products;
-        }
-      }
+      produtosCat = repositoryProduct.products
+          .where((product) => product.category == itemSelecionado.name);
+    }
+
+    Future<bool> adicionarProduto(BuildContext context) async {
+      return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (_) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // SizedBox(height: 10),
+              // //dropDownCAtegoria(categorias),
+              // SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Produto",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              // SizedBox(height: 10),
+              FlatButton(
+                child: Text("Ok"),
+                onPressed: () => Navigator.of(context).pop(true),
+              ),
+            ],
+          );
+        },
+      );
     }
 
     return Scaffold(
@@ -51,120 +69,185 @@ class _StockState extends State<Stock> {
           color: Colors.black,
           opacity: .4,
         ),
-        foregroundColor: Colors.black,
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xff940C0C),
         title: Text(
           'Estoque',
           style: GoogleFonts.ubuntu(
-            color: Colors.black,
+            color: Colors.white,
           ),
         ),
+        actions: [
+          IconButton(
+              iconSize: 30,
+              padding: EdgeInsets.only(right: 20),
+              icon: Icon(
+                Icons.home,
+              ),
+              hoverColor: Colors.white,
+              splashColor: Colors.white,
+              highlightColor: Colors.white,
+              onPressed: () {} //=> adicionarProduto(context),
+              ),
+        ],
       ),
       body: Column(
         children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-            padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(11.36),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 2,
-                    color: Colors.blue,
-                    offset: Offset(1, 4),
-                  ),
-                ]),
-            child: DropdownButtonHideUnderline(
-              child: DropdownButton(
-                value: (itemSelecionado == null)
-                    ? itemSelecionado
-                    : itemSelecionado.name,
-                hint: Text(
-                  'Selecione a categoria',
-                  style: GoogleFonts.ubuntu(
-                    color: Colors.black,
-                  ),
-                ),
-                icon: Icon(
-                  Icons.arrow_drop_down,
-                  size: 30,
-                ),
-                isExpanded: true,
-                style: TextStyle(color: Colors.black, fontSize: 16),
-                items:
-                    categorias.map<DropdownMenuItem<String>>((Categoria value) {
-                  return DropdownMenuItem<String>(
-                    value: value.name,
-                    child: Text(value.name),
-                  );
-                }).toList(),
-                onChanged: (var newValue) {
-                  setState(() {
-                    itemSelecionado =
-                        categorias.firstWhere((cat) => cat.name == newValue);
-                    produtos.clear();
-                  });
-                },
-              ),
-            ),
-          ),
+          dropDownCAtegoria(categorias),
           Divider(),
           Expanded(
-            child: ListView.builder(
-              itemCount: produtos.length,
-              itemBuilder: (_, index) {
-                var nome = produtos[index];
-                return Dismissible(
-                  key: Key(nome.name),
-                  child: Container(
-                    margin: EdgeInsets.only(left: 30, right: 30, top: 10),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(11.36),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            blurRadius: 2,
-                            offset: Offset(1, 3),
-                          ),
-                        ]),
-                    child: ListTile(
-                      title: Text(
-                        nome.name,
-                        style: GoogleFonts.ubuntu(
-                          color: Colors.black,
-                        ),
-                      ),
-                      subtitle: Text(
-                        nome.price.toString(),
-                        style: GoogleFonts.ubuntu(
-                          color: Colors.black,
-                        ),
-                      ),
-                      trailing: Container(
-                        width: 20,
-                        child: Row(
-                          children: [
-                            Text(
-                              nome.amount.toString(),
+            child: (itemSelecionado == null)
+                ? ListView.builder(
+                    itemCount: produtos.length,
+                    itemBuilder: (_, index) {
+                      var nome = produtos[index];
+                      return Dismissible(
+                        key: Key(nome.name),
+                        child: Container(
+                          margin: EdgeInsets.only(left: 30, right: 30, top: 10),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(11.36),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.3),
+                                  blurRadius: 2,
+                                  offset: Offset(1, 3),
+                                ),
+                              ]),
+                          child: ListTile(
+                            title: Text(
+                              nome.name,
                               style: GoogleFonts.ubuntu(
                                 color: Colors.black,
-                                fontSize: 16,
                               ),
-                            )
-                          ],
+                            ),
+                            subtitle: Text(
+                              nome.price.toString(),
+                              style: GoogleFonts.ubuntu(
+                                color: Colors.black,
+                              ),
+                            ),
+                            trailing: Container(
+                              width: 20,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    nome.amount.toString(),
+                                    style: GoogleFonts.ubuntu(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
+                      );
+                    },
+                  )
+                : ListView.builder(
+                    itemCount: produtosCat.length,
+                    itemBuilder: (_, index) {
+                      var nome = produtosCat.toList()[index];
+                      return Dismissible(
+                        key: Key(nome.name),
+                        child: Container(
+                          margin: EdgeInsets.only(left: 30, right: 30, top: 10),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(11.36),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.3),
+                                  blurRadius: 2,
+                                  offset: Offset(1, 3),
+                                ),
+                              ]),
+                          child: ListTile(
+                            title: Text(
+                              nome.name,
+                              style: GoogleFonts.ubuntu(
+                                color: Colors.black,
+                              ),
+                            ),
+                            subtitle: Text(
+                              nome.price.toString(),
+                              style: GoogleFonts.ubuntu(
+                                color: Colors.black,
+                              ),
+                            ),
+                            trailing: Container(
+                              width: 20,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    nome.amount.toString(),
+                                    style: GoogleFonts.ubuntu(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
           ),
         ],
       ),
       bottomNavigationBar: BottomNaviBar(),
+    );
+  }
+
+  Container dropDownCAtegoria(List<Categoria> categorias) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(11.36),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 2,
+              color: Colors.blue,
+              offset: Offset(1, 4),
+            ),
+          ]),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton(
+          value: (itemSelecionado == null)
+              ? itemSelecionado
+              : itemSelecionado.name,
+          hint: Text(
+            'Selecione a categoria',
+            style: GoogleFonts.ubuntu(
+              color: Colors.black,
+            ),
+          ),
+          icon: Icon(
+            Icons.arrow_drop_down,
+            size: 30,
+          ),
+          isExpanded: true,
+          style: TextStyle(color: Colors.black, fontSize: 16),
+          items: categorias.map<DropdownMenuItem<String>>((Categoria value) {
+            return DropdownMenuItem<String>(
+              value: value.name,
+              child: Text(value.name),
+            );
+          }).toList(),
+          onChanged: (var newValue) {
+            setState(() {
+              itemSelecionado =
+                  categorias.firstWhere((cat) => cat.name == newValue);
+            });
+          },
+        ),
+      ),
     );
   }
 }
