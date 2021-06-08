7e71d0a17191793914ffeaa793059c23e849a8bd
Guilherme Vinicius <downnxd@hotmail.com>
2021-06-08 15:46:24 -0300

    Revert "Alterações no login"

--- lib/views/Home/home.dart
+++ lib/views/Home/home.dart
@@ -1,47 +1,21 @@
-import 'dart:convert';
-import 'package:badydoces/models/produto.model.dart';
-import 'package:provider/provider.dart';
-
-import 'package:badydoces/models/admin.model.dart';
-import 'package:badydoces/repositories/produto_repository.dart';
-import 'package:badydoces/views/Home/home_controller.dart';
 import 'package:badydoces/views/Home/widgets/estoque_alerta/estoque_alerta_widget.dart';
 import 'package:badydoces/views/Home/widgets/total_vendas_card/total_vendas_card_widget.dart';
-import 'package:badydoces/views/auth/AuthController.dart';
 import 'package:badydoces/views/components/bottomNaviBar/index.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../components/bottomNaviBar/index.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final HomeController homeController = HomeController();
-  final AuthController authController = AuthController();
-
-  @override
-  void initState() {
-    authController.autenticar();
-    homeController.fetchProducts();
-  }
-
   Widget build(BuildContext context) {
-    HomeController _homeController = Provider.of<HomeController>(context);
-    AuthController _authController = Provider.of<AuthController>(context);
-
-    List<Product> prodcuts = _homeController.products;
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
         backgroundColor: Colors.white,
         title: Text(
-          'Bady Doces logado email',
+          'Bady Doces',
           style: GoogleFonts.ubuntu(
             color: Colors.black,
           ),
